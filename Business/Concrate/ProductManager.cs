a0ee977a4f3d9639652628b927a4f67d1250255d
cagricandan <cgrcandan@gmail.com>
2021-02-27 11:56:42 +0300

    Add project files.

---
+++ Business/Concrate/ProductManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrate.InMemory;
+using Entities.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrate
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _ıProductDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _ıProductDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            //iş kodları kontrol
+            //yetkilimi
+            return _ıProductDal.GetAll();
+        }
+    }
+}
